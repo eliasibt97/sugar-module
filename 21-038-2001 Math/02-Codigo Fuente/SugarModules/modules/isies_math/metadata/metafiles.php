900019d9cd4713f802c6485b49e0935b28b44049
Eliasib Toris <eliasibt97@gmail.com>
2021-06-17 15:31:27 -0500

    Search Config updates

--- 21-038-2001 Math/02-Codigo Fuente/SugarModules/modules/isies_math/metadata/metafiles.php
+++ 21-038-2001 Math/02-Codigo Fuente/SugarModules/modules/isies_math/metadata/metafiles.php
@@ -4,7 +4,7 @@
         'detailviewdefs'  =>    'modules/' . $module_name . '/metadata/detailviewdefs.php',
         'editviewdefs'    =>    'modules/' . $module_name . '/metadata/editviewdefs.php',
         'listviewdefs'    =>    'modules/' . $module_name . '/metadata/listviewdefs.php',
-        //'searchdefs'      =>    'modules/' . $module_name . '/metadata/searchdefs.php',
+        'searchdefs'      =>    'modules/' . $module_name . '/metadata/searchdefs.php',
         //'searchfields'    =>    'modules/' . $module_name . '/metadata/SearchFields.php',
      );
 
