062c7e66aa8f257e7c3c0067def8c4148665e7bf
Eliasib Toris <eliasibt97@gmail.com>
2021-06-17 08:26:44 -0500

    Views Settings 0.0.1

--- 21-038-2001 Math/02-Codigo Fuente/SugarModules/modules/isies_math/metadata/listviewdefs.php
+++ 21-038-2001 Math/02-Codigo Fuente/SugarModules/modules/isies_math/metadata/listviewdefs.php
@@ -4,25 +4,25 @@ $module_name = 'isies_math';
 $listViewDefs[$module_name] = [
     'Primer numero' => [
         'width' => '25%',
-        'label' => 'Primer Numero',
+        'label' => 'LBL_NUMERO_UNO',
         'dafault' => true,
         'link' => true
     ],
     'Segundo numero' => [
         'width' => '25%',
-        'label' => 'Segundo Numero',
+        'label' => 'LBL_NUMERO_DOS',
         'dafault' => true,
         'link' => true
     ],
     'Resultado' => [
         'width' => '25%',
-        'label' => 'Resultado',
+        'label' => 'LBL_RESULTADO',
         'dafault' => true,
         'link' => true
     ],
     'Tipo' => [
         'width' => '25%',
-        'label' => 'Tipo',
+        'label' => 'LBL_TIPO',
         'dafault' => true,
         'link' => true
     ],
