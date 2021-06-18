9d4836db4a9b22a8ba5a77b34bafc22bcfac73a7
Eliasib Toris <eliasibt97@gmail.com>
2021-06-18 09:53:12 -0500

    Vardefs updates

--- 21-038-2001 Math/02-Codigo Fuente/SugarModules/modules/isies_math/metadata/listviewdefs.php
+++ 21-038-2001 Math/02-Codigo Fuente/SugarModules/modules/isies_math/metadata/listviewdefs.php
@@ -2,29 +2,35 @@
 
 $module_name = 'isies_math';
 $listViewDefs[$module_name] = array(
+    'NAME' => array(
+        'width' => '25',
+        'label' => 'LBL_NAME',
+        'dafault' => true,
+        'link' => true,
+    ),
     'NUMERO_UNO' => array(
         'width' => '25',
         'label' => 'LBL_NUMERO_UNO',
         'dafault' => true,
-        'link' => true
+        'link' => true,
     ),
     'NUMERO_DOS' => array(
         'width' => '25',
         'label' => 'LBL_NUMERO_DOS',
         'dafault' => true,
-        'link' => true
+        'link' => true,
     ),
     'RESULTADO' => array(
         'width' => '25',
         'label' => 'LBL_RESULTADO',
         'dafault' => true,
-        'link' => true
+        'link' => true,
     ),
     'TIPO' => array(
         'width' => '25',
         'label' => 'LBL_TIPO',
         'dafault' => true,
-        'link' => true
+        'link' => true,
     ),
 );
 
